6d30b23c4dbba22794e24b05bdafbbab9b2b088e
Ioane Mania <imaniaioane@gmail.com>
2024-12-15 22:47:39 +0400

    inital setup with lazy, treesitter, lsp-config and telescope

---
+++ init.lua
@@ -0,0 +1,19 @@
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<cr>")
+vim.keymap.set("n", "<space>x", ":.lua<cr>")
+vim.keymap.set("v", "<space>x", ":lua<cr>")
+
+-- Highlight when yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
---
+++ lua/config/plugins/lspconfig.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup {}
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup {
+        ensure_installed = { "lua_ls" }
+      }
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
+      require("lspconfig").lua_ls.setup {}
+
+      vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format() end)
+    end,
+  },
+}

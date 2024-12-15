12bbc68b0aeb03b176b0f8e02355920bb642d7c8
Ioane Mania <imaniaioane@gmail.com>
2024-12-15 23:17:31 +0400

    add more mini features

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -4,6 +4,19 @@ return {
     config = function()
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = true }
+
+      require('mini.notify').setup {}
+      require('mini.pairs').setup {}
+      require('mini.starter').setup {}
+      require('mini.surround').setup {}
+      require('mini.trailspace').setup {}
+      require('mini.comment').setup {}
+
+      require('mini.diff').setup {
+        view = { style = 'sign' }
+      }
+
+      vim.keymap.set("n", "<space>go", ":lua MiniDiff.toggle_overlay()<cr>")
     end
   },
 }

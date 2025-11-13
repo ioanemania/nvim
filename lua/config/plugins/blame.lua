ffad7794ce298975c6fc05a40d2f79980fc30d8d
Ioane Mania <imaniaioane@gmail.com>
2025-11-13 15:26:07 +0400

    add git blame

---
+++ lua/config/plugins/blame.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "FabijanZulj/blame.nvim",
+    lazy = false,
+    config = function()
+      require('blame').setup {}
+
+      vim.keymap.set("n", "<leader>gb", "<cmd>BlameToggle<cr>")
+    end,
+  },
+}

dadf1ba1f0d7f2723968e230683205ea720a2901
Ioane Mania <imaniaioane@gmail.com>
2025-11-13 15:28:54 +0400

    add outline.nvim

---
+++ lua/config/plugins/outline.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+        { desc = "Toggle Outline" })
+
+      require("outline").setup {
+        outline_window = {
+          position = 'left'
+        }
+      }
+    end,
+  },
+}

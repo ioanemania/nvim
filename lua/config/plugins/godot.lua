60de42e748f2113d349c3ea2fc3e0b18a9ae6ee9
Ioane Mania <imaniaioane@gmail.com>
2026-03-25 18:27:56 +0400

    disable godotdev.nvim

--- lua/config/plugins/godot.lua
+++ lua/config/plugins/godot.lua
@@ -1,2 +1,11 @@
 return {
+  -- {
+  --   'Mathijs-Bakker/godotdev.nvim',
+  --   dependencies = { 'nvim-lspconfig', 'nvim-treesitter' },
+  --   config = function()
+  --     require("godotdev").setup({
+  --       -- autostart_editor_server = true,
+  --     })
+  --   end
+  -- }
 }

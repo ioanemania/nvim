ed9f43690c5688d184b558a3cebeacbbef7f8515
Ioane Mania <imaniaioane@gmail.com>
2026-03-25 18:26:54 +0400

    add neovide options and transparent telescope

--- init.lua
+++ init.lua
@@ -1,5 +1,28 @@
 require("config.lazy")
 
+if vim.g.neovide then
+  vim.g.neovide_opacity = 0.7
+  vim.g.neovide_normal_opacity = 0.7
+  vim.g.neovide_window_blurred = true
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    -- Make Telescope transparent
+    vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { bg = 'none' })
+    
+    -- Optional: If using other floating windows
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+  end,
+})
+
+
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.clipboard = "unnamedplus"

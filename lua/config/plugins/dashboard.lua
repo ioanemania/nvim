54b7f68ab2949cec93133df5ca205cb223fba28d
Ioane Mania <imaniaioane@gmail.com>
2026-06-19 17:03:06 +0400

    disable dashboard-nvim

--- lua/config/plugins/dashboard.lua
+++ lua/config/plugins/dashboard.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        theme = 'doom',
-        config = {
-          vertical_center = true,
-          center = {
-            { desc = "Find File", key = "f", action = "Telescope find_files previewer=false cwd="},
-            { desc = "Find Text", key = "g", action = "Telescope live_grep"}
-          }
-        }
-      }
-    end,
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
-  }
+  -- {
+  --   'nvimdev/dashboard-nvim',
+  --   event = 'VimEnter',
+  --   config = function()
+  --     require('dashboard').setup {
+  --       theme = 'doom',
+  --       config = {
+  --         vertical_center = true,
+  --         center = {
+  --           { desc = "Find File", key = "f", action = "Telescope find_files previewer=false cwd="},
+  --           { desc = "Find Text", key = "g", action = "Telescope live_grep"}
+  --         }
+  --       }
+  --     }
+  --   end,
+  --   dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+  -- }
 }

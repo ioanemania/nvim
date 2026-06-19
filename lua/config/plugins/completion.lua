cf228ff70ba1394a4d9c74d0ea78cc7674ce50ec
Ioane Mania <imaniaioane@gmail.com>
2026-06-19 17:04:46 +0400

    modify completion menu layout

--- lua/config/plugins/completion.lua
+++ lua/config/plugins/completion.lua
@@ -6,9 +6,15 @@ return {
     opts = {
       keymap = { preset = 'default' },
 
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = 'mono'
+      completion = {
+        menu = {
+          draw = {
+            columns = {
+              { "label",     "label_description", gap = 1 },
+              { "kind_icon", gap = 1, "kind" }
+            },
+          },
+        },
       },
 
       signature = { enabled = false }

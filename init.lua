8a6dcc1404f6c222e6177f9dfa51ea2c3ab8910b
Ioane Mania <imaniaioane@gmail.com>
2026-06-25 14:51:27 +0400

    migrate treesitter to main branch version

--- init.lua
+++ init.lua
@@ -53,3 +53,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'python', 'lua', 'rust' },
+  callback = function() vim.treesitter.start() end,
+})
+
+vim.cmd("colorscheme gruvbox")
